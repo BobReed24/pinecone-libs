7c5f53c3ec8ed4b74472e8af1a330651947fb49b
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-24 09:59:14 -0600

    Update battery.py

--- battery.py
+++ battery.py
@@ -1,6 +1,6 @@
+ver = "V1.0.0"
 import subprocess
 import os
-ver = "V1.0.0"
 pre_process = "sudo apt-get -y install acpi"
 process = subprocess.run(pre_process, shell=True, capture_output=True, text=True)
 os.system("acpi")
